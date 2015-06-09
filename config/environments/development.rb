aee823d61b6d6caf46730e3e0deb744321bba0c5
Masato Suzuki <masatos7@gmail.com>
2015-06-10 07:21:50 +0900

    aaa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-
+ttt
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
