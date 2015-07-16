52a3c1ddf44f64cb2e4102215914fbe9eeb5272d
Masato Suzuki <masatos7@gmail.com>
2015-07-17 08:00:23 +0900

    Add UserDeathEdit
    Added user/death/edit. Still need to fix user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
 	def show
-    @user = User.find(current_user.id)
   end
   def death_show
     @user = User.find(current_user.id)
   end
+  def death_edit
+    @user = User.find(current_user.id)
+  end
+  def death_update
+    @user = User.find(current_user.id)
+
+    if @user.update_attributes(user_params)
+      flash[:notice] = "Subject update sccessfully."
+      redirect_to(:action => 'death_show')
+    else
+      render('death_edit')
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:count,:interval)
+    end
 end
